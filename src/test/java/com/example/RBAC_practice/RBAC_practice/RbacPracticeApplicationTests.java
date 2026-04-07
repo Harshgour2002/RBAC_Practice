ccaaa905249d3fcee99dd2969f004993e63ca2ad
Harsh Gour <gourharsh8909@gmail.com>
2026-04-07 14:43:19 +0530

    first commit

---
+++ src/test/java/com/example/RBAC_practice/RBAC_practice/RbacPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RBAC_practice.RBAC_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
